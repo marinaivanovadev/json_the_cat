7037c6e693c7278f98d7615449dde0d7b02d74b1
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-06-16 23:07:01 -0700

    Refactoring

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,29 +1,32 @@
 const request = require('request'); // request library
 // access to command line for Node.js
 
-const breedName = process.argv[2]; // the third element
-// console.log(breedName);
+const fetchBreedDescription = function(breedName, callback) {
+  if (!breedName) {
+    callback("Please include the search breed name", null);
+  } else {
+    const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+    // request to check loads is fine
 
-if (!breedName) {
-  console.log("Please include the search breed name");
-} else {
-  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-  // request to check loads is fine
+    request(url, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        //console.log(`Request loaded`);
 
-  request(url, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-    //console.log(`Request loaded`);
-
-      //console.log(typeof body); // check the type of body - string
-      const data = JSON.parse(body);// convert from string to object
-      //console.log(typeof data); // confirm that it is object
-      if (Array.isArray(data) && data.length > 0) {
-        console.log(data[0].description); // object from site, access to object in the array
+        //console.log(typeof body); // check the type of body - string
+        const data = JSON.parse(body);// convert from string to object
+        //console.log(typeof data); // confirm that it is object
+        if (Array.isArray(data) && data.length > 0) {
+          callback(null, data[0].description); // object from site, access to object in the array
+        } else {
+          callback("No breed found", null); // Edge Case: Breed Not Found
+        }
       } else {
-        console.log("No breed found"); // Edge Case: Breed Not Found
+        callback("Error to load", null); // Edge Case: Request Failed
       }
-    } else {
-      console.log("Error to load", error); // Edge Case: Request Failed
-    }
-  });
-}
+    });
+  }
+};
+
+module.exports = {
+  fetchBreedDescription
+};
