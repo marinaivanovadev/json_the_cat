c6d94244a88c84fe63993bac4e3a59467dfa1458
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-06-16 22:43:00 -0700

    Update with edges

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,26 +4,26 @@ const request = require('request'); // request library
 const breedName = process.argv[2]; // the third element
 // console.log(breedName);
 
-if (breedName === undefined) {
+if (!breedName) {
   console.log("Please include the search breed name");
-}
+} else {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  // request to check loads is fine
 
-// request to check loads is fine
-
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
+  request(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
     //console.log(`Request loaded`);
 
-    //console.log(typeof body); // check the type of body - string
-    const data = JSON.parse(body);// convert from string to object
-    console.log(data[0].description); // object from site, access to object in the array
-    //console.log(typeof data); // confirm that it is object
-    
-
-  } else if (breedName === undefined) {
-    console.log("Please include the search breed name");
-  } else {
-    console.log("Error to load", error);
-  }
-});
-
+      //console.log(typeof body); // check the type of body - string
+      const data = JSON.parse(body);// convert from string to object
+      //console.log(typeof data); // confirm that it is object
+      if (Array.isArray(data) && data.length > 0) {
+        console.log(data[0].description); // object from site, access to object in the array
+      } else {
+        console.log("No breed found"); // Edge Case: Breed Not Found
+      }
+    } else {
+      console.log("Error to load", error); // Edge Case: Request Failed
+    }
+  });
+}
