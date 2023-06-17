d130931278ee2d8d8f7e8e62bd5df52954537f29
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-06-16 22:31:51 -0700

    Edge: nNo search name

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,13 +2,17 @@ const request = require('request'); // request library
 // access to command line for Node.js
 
 const breedName = process.argv[2]; // the third element
-//console.log(breedName);
+// console.log(breedName);
+
+if (breedName === undefined) {
+  console.log("Please include the search breed name");
+}
 
 // request to check loads is fine
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
   if (!error && response.statusCode === 200) {
-    console.log(`Request loaded`);
+    //console.log(`Request loaded`);
 
     //console.log(typeof body); // check the type of body - string
     const data = JSON.parse(body);// convert from string to object
@@ -16,6 +20,8 @@ request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, res
     //console.log(typeof data); // confirm that it is object
     
 
+  } else if (breedName === undefined) {
+    console.log("Please include the search breed name");
   } else {
     console.log("Error to load", error);
   }
